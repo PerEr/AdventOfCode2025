87e31a52b41de544d87a71be5f2e6cbd32db42b7
Per Ersson <per.ersson@gmail.com>
2025-12-02 21:02:18 +0100

    Refactored day 1 solution

--- src/1.ts
+++ src/1.ts
@@ -6,16 +6,14 @@ const rotations = readFileSync("data/1.txt", "utf8").split("\n").map((l) => {
 })
 
 console.log("Part1: ", rotations.reduce(({ position, hits }, value) => {
-  const newPosition = position + value;
-  const newHits = newPosition % 100 === 0 ? hits + 1 : hits;
-  return { position: newPosition, hits: newHits };
+  position += value;
+  if (position % 100 === 0) hits += 1;
+  return { position, hits };
 }, { position: 50, hits: 0 }).hits);
 
 console.log("Part2: ", rotations.reduce(({ position, hits }, value) => {
-  const delta = Math.sign(value);
-  const steps = Math.abs(value);
   for (let ii=0 ; ii<Math.abs(value); ii += 1) {
-    position += delta;
+    position += Math.sign(value);
     if (position < 0) position = 99;
     if (position % 100 === 0) hits++;
   }

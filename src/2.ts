a95f233fdfde3d349012ba6d926eaffaffae8a85
Per Ersson <per.ersson@gmail.com>
2025-12-02 20:25:21 +0100

    Day 2, part2.

--- src/2.ts
+++ src/2.ts
@@ -18,4 +18,25 @@ const findInvalids = (pair: {l: number, h: number}) => {
     return invalids;
 }
 
+const findInvalids2 = (pair: {l: number, h: number}) => {
+    let invalids: Array<number> = [];
+    for (let ii=pair.l; ii<=pair.h; ii++) {
+        const str = ii.toString();
+        for (let jj=1; jj<=str.length/2; jj++) {
+            const pattern = str.slice(0, jj);
+            let repeatPattern = "";
+            while (repeatPattern.length < str.length) {
+                repeatPattern += pattern;
+            }
+            if (repeatPattern == str) {
+                invalids.push(ii);
+                break;
+            }
+        }
+    }
+    return invalids;
+}
+
+
 console.log("Part1: ", ranges.flatMap(findInvalids).reduce((a, b) => a + b, 0));
+console.log("Part2: ", ranges.flatMap(findInvalids2).reduce((a, b) => a + b, 0));

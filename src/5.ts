9329bf1e94197cfb0d037db5976317450bb1571a
Per Ersson <per.ersson@gmail.com>
2025-12-05 08:38:14 +0100

    Day 5, part 2.

--- src/5.ts
+++ src/5.ts
@@ -4,3 +4,24 @@ const [r, ingredients] = readFileSync("data/5.txt", "utf8").split("\n\n").slice(
 const ranges = r.split("\n").map(line => line.split("-").map(Number));
 
 console.log("Part1:", ingredients.split("\n").map(Number).filter((n) => ranges.some(([start, end]) => n >= start && n <= end)).length);
+
+console.log("Part2:", ranges.flatMap(([start, end]) =>
+    [[start, 1], [end, 0]]
+).sort((a: number[], b: number[]) => {
+    if (a[0] !== b[0]) return a[0] - b[0];
+    return b[1] - a[1];
+}).reduce(({ count, depth, prev }, [pos, type]) => {
+    if (type === 1) {
+        if (depth === 0) {
+            prev = pos;
+        }
+        depth += 1;
+    } else {
+        depth -= 1;
+        if (depth === 0) {
+            count += (pos - prev + 1);
+        }
+    }
+    return { count, depth, prev };
+}, { count: 0, depth: 0, prev: 0 }).count);
+

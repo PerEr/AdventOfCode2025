ffb57508368e958465d5fc248fa17f5b5d7947e1
Per Ersson <per.ersson@gmail.com>
2025-12-07 10:56:29 +0100

    Day 7, part 2.

--- src/7.ts
+++ src/7.ts
@@ -25,3 +25,17 @@ const findSplits = ({ r, c }: Pos, splits = new Set<string>()): number => {
 }
 
 console.log("Part1: ", findSplits(startPos));
+
+const findQuantumPaths = ({ r, c }: Pos, cache = new Map<string, number>()): number => {
+    if (r >= grid.length) return 1;
+    if (c < 0 || c >= grid[r].length) return 0;
+    const key = posToKey({ r, c });
+    if (cache.has(key)) return cache.get(key)!;
+    const res = grid[r][c] === '^'
+        ? findQuantumPaths({ r, c: c - 1 }, cache) + findQuantumPaths({ r, c: c + 1 }, cache)
+        : findQuantumPaths({ r: r + 1, c }, cache);
+    cache.set(key, res);
+    return res;
+};
+
+console.log("Part2: ", findQuantumPaths(startPos));

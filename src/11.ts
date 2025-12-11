1a465c556dc8b0dc63510f32af00d9dc3478f0f3
Per Ersson <per.ersson@gmail.com>
2025-12-11 21:34:43 +0100

    Day 11, part 2.

--- src/11.ts
+++ src/11.ts
@@ -1,22 +1,17 @@
 import { readFileSync } from "fs";
 
-const nodes: Map<string, string[]> = readFileSync("data/11.txt", "utf8").split("\n").reduce((n, l) => {
+const graph: Map<string, string[]> = readFileSync("data/11.txt", "utf8").split("\n").reduce((n, l) => {
     const parts = l.split(": ");
     n.set(parts[0], parts[1].split(' '))
     return n;
 }, new Map<string, string[]>());
 
-const countPaths = (
-    graph: Map<string, string[]>,
-    start: string,
-    target: string
-): number => {
+const countPaths = (start: string, target: string): number => {
     const cache = new Map<string, number>();
 
     function dfs(node: string): number {
         if (node === target) return 1;
         if (cache.has(node)) return cache.get(node)!;
-
         const paths = (graph.get(node) || []).reduce((s, n) => s + dfs(n), 0);
         cache.set(node, paths);
         return paths;
@@ -25,4 +20,11 @@ const countPaths = (
     return dfs(start);
 }
 
-console.log('Part1:', countPaths(nodes, 'you', 'out'));
+console.log('Part1:', countPaths('you', 'out'));
+
+const countPathsVia = (start: string, target: string, via1: string, via2: string): number =>
+    countPaths(start, via1) * countPaths(via1, via2) * countPaths(via2, target) +
+    countPaths(start, via2) * countPaths(via2, via1) * countPaths(via1, target);
+
+
+console.log('Part2:', countPathsVia('svr', 'out', 'dac', 'fft'));
